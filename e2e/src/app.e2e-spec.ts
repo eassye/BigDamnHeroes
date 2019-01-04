9119fe98275020cca509cf256197f66169edf382
eassye <eassye1@yahoo.com>
2019-01-03 19:12:03 -0600

    renamed project and all references to old name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to BigDamnHeros!');
+    expect(page.getTitleText()).toEqual('Welcome to BigDamnHeroes!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BigDamnHeros'`, () => {
+  it(`should have as title 'BigDamnHeroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('BigDamnHeros');
+    expect(app.title).toEqual('BigDamnHeroes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to BigDamnHeros!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to BigDamnHeroes!');
   });
 });

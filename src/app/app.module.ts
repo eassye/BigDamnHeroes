3d6196a15b9720d78845f3ff24fc7364463d346d
eassye <eassye1@yahoo.com>
2019-01-08 21:18:59 -0600

    created big-damn-heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'
 import { SharedModule } from "./shared/shared.module";
 import { SpeciesComponent } from './wrath-and-glory/character-creation/species/species.component';
+import { HomeComponent } from './big-damn-heroes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpeciesComponent
+    SpeciesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
